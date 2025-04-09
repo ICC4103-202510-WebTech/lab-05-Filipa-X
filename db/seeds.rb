96b0029c48f2cdfaeeb5967ea35f22cf06f3532d
Filipa-X <fxparada@miuandes.cl>
2025-04-09 16:09:45 -0400

    LAB5

---
+++ db/seeds.rb
@@ -0,0 +1,37 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+Message.destroy_all
+Chat.destroy_all
+User.destroy_all
+
+10.times do |i|
+    User.create!(
+        email: "user#{i + 1}@mail.com",
+        first_name: "first_name #{i + 1}",
+        last_name: "last_name #{i + 1}",
+
+    
+    )
+end
+
+10.times do |i| 
+    Chat.create!(
+        sender_id: rand(1..10),
+        receiver_id: rand(1..10)
+    )
+end
+
+10.times do |i|
+    Message.create!(
+        chat_id: rand(1..10),
+        user_id: rand(1..10),
+        body: "This is message number #{i + 1}"
+    )
+end
